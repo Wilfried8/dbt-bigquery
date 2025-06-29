deb637e0e13c16fa57e73ff47d979adf9a31105d
wilfried <wilfried.tayo@groupe-esigelec.org>
2025-06-29 17:52:32 +0200

    feat: add sqlfluff conf + staging + intermediaire

--- dbt_bigquery_elt/models/staging/customers/stg_customers.sql
+++ dbt_bigquery_elt/models/staging/customers/stg_customers.sql
@@ -1,12 +1,12 @@
-select 
+select
     customer_id,
+    phone,
+    state,
+    zip_code,
     lower(trim(first_name)) as first_name,
     lower(trim(last_name)) as last_name,
     lower(email) as email,
-    phone,
     lower(street) as street,
     lower(city) as city,
-    state,
-    zip_code,
     safe_cast("{{ var("created_at") }}" as timestamp) as created_at
 from {{ source('raw', 'raw_customers') }}
